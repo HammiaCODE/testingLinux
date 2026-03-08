801dbbee804302db9d597a4c532053130f672291
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-03-08 14:03:52 -0600

    Added log

---
+++ helloLinux.py
@@ -0,0 +1,9 @@
+import datetime
+
+log = datetime.datetime.now()
+print("Hello Linux!")
+print("LOG:", log)
+print(" ")
+print("This is T24's test log")
+print("Members: ")
+print ("Santiago Alonzo Aguilar A01639373", "/ Hannia Escamilla Pérez A01639113", "/ Cared Nicolle Castaños Manjarrez A01742620", "/ Patricio Flores Reynoso A01645013")
